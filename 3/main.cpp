2ad8008749565d706de21bfc3d53c5002ab75591
Tom Lausberg <toml@ethz.ch>
2023-01-04 11:27:01 +0100

    finish 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -7,11 +7,26 @@
 
 std::string sortString(std::string str)
 {
-   sort(str.begin(), str.end());
-
+    sort(str.begin(), str.end());
     return str;
 } 
- 
+
+
+
+
+int getPriority(char letter)
+{
+    int priority = 0;
+    if(letter >= 'a' && letter <= 'z'){
+        // if the letter is lowercase
+        priority = letter - 96;
+    } else if (letter >= 'A' && letter <= 'Z') {
+        // if the letter is uppercase
+        priority = letter - 38;
+    }
+    // std::cout << "Letter: " << letter << " Priority: " << priority << std::endl; 
+    return priority;
+}
 
 int getSumOfPriorities(std::string filename)
 {
@@ -28,20 +43,11 @@ int getSumOfPriorities(std::string filename)
         int j = 0;
         while( i < compartmentSize && j < compartmentSize){
             if(compartment1[i] < compartment2[j]){
-                sum += compartment1[i] - 38;
                 i++;
             }else if (compartment2[j] < compartment1[i]){
                 j++;
-            } else if (compartment1[i] < 'a') {
-                std::cout << " " << lineCount << " " << sum << " " << compartment1[i] << " " << compartment2[j] << " " <<compartment1[i] - 38 << std::endl;
-
-                // if the letter is uppercase
-                sum += compartment1[i] - 38;
-                break;
-                //
-            } else {
-                std::cout << " " << lineCount << " " << sum << " " << compartment1[i] << " " << compartment2[j] << " " <<compartment1[i] - 96 << std::endl;
-                sum += compartment1[i] - 96;
+            } else if (compartment1[i] == compartment2[j]) {
+                sum += getPriority(compartment1[i]);
                 break;
             }
         }
@@ -49,10 +55,40 @@ int getSumOfPriorities(std::string filename)
     }
     return sum;
 }
+
+
+int sumOfBadges(std::string filename)
+{
+    std::ifstream file(filename);
+    int sum = 0;
+    std::string elf1;
+    std::string elf2;
+    std::string elf3;
+
+    while(true){
+        getline(file, elf1);
+        getline(file, elf2);
+        getline(file, elf3);
+        
+        for(char badge : elf1){
+            if(elf2.find(badge) != std::string::npos && elf3.find(badge) != std::string::npos){
+                sum += getPriority(badge);
+                break;
+            }
+        }
+        if(file.eof()){
+            break;
+        }
+    }
+    return sum;
+}
+
 int main(int argc, char const *argv[])
 {
     // read input file
     int prioritySum = getSumOfPriorities("input.txt");
     std::cout << "Sum of priorities: " << prioritySum << std::endl;
+    int badgeSum = sumOfBadges("input.txt");
+    std::cout << "Sum of badges: " << badgeSum << std::endl;
     return 0;
 }
